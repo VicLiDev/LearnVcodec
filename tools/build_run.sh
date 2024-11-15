06f4ac68d425408d1d26f42c145f06e106116658
Hongjin Li <vic.hong@rock-chips.com>
2024-11-15 17:58:57 +0800

    update build tool

--- tools/build_run.sh
+++ tools/build_run.sh
@@ -1,90 +1,89 @@
 #!/bin/bash
 
-# usage: build_run.sh <prot> [<b>] [<bt>] [<gdb>]
-
 clear
+
+# single run cmd
+sig_hevc_run=""
+sig_avc_run=""
+sig_avs2_run=""
+sig_av1_run="./aomdec -o output.yuv ${HOME}/test/testStrms/Sintel_360_10s_1MB.ivf"
+sig_vp9_run=""
 
 # ============ base ============
 curScriptPath=`dirname $0`
-echo "curScriptPath: $curScriptPath"
 curPath=`pwd`
-echo "curPath: $curPath"
 curDir=${curPath#*LearnVcodec/}
-echo "curDir: $curDir"
 rootPath=${curPath%/$curDir}
+echo "============> path <============"
+echo "curScriptPath: $curScriptPath"
 echo "rootPath: $rootPath"
+echo "curPath: $curPath"
+echo "curDir: $curDir"
+echo "============> path <============"
 
 cmd_prot="null"
 cmd_build=false
+cmd_sig_test=false
+cmd_batch_test=false
 cmd_debug=false
+
+function help()
+{
+    echo "usage: build_run.sh <-p prot> <-b|-r|-bt> [<-gdb>]"
+    echo "    -p:   prot, hevc|avc|avs2|av1|vp9"
+    echo "    -b:   build"
+    echo "    -r:   run single test"
+    echo "    -bt:  batch test"
+    echo "    -gdb: debug"
+}
 
 function procParas()
 {
-    for para in $*
-    do
-        case ${para} in
-        "hevc" | "avc" | "avs2" | "av1" | "vp9")
-            cmd_prot=${para}
-            ;;
-        "b")
-            cmd_build=true
-            ;;
-        "gdb")
-            cmd_debug=true
-            ;;
-        *)
-            echo "unsupport cmd"
-            ;;
+    while [[ $# -gt 0 ]]; do
+        key="$1"
+        case ${key} in
+            -p)   cmd_prot=${2}; shift ;;
+            -b)   cmd_build=true ;;
+            -r)   cmd_sig_test=true ;;
+            -bt)  cmd_batch_test=true ;;
+            -gdb) cmd_debug=true ;;
+            -h)   help; exit 0 ;;
+            *)    echo "unsupport cmd ${key}"; help; exit 1 ;;
         esac
+        shift # move to next para
     done
 
-    if [ $cmd_prot == "null" ]; then
-        echo "prot is null"
-    fi
 
-    echo "======> cmd <======"
-    echo "prot: $cmd_prot"
-    echo "build: $cmd_build"
-    echo "debug: $cmd_debug"
-    echo "======> cmd <======"
+    [ $cmd_prot == "null" ] && { echo "Error: prot is null"; help; exit;}
+
+    echo "============> cmd <============"
+    echo "prot:       ${cmd_prot}"
+    echo "build:      ${cmd_build}"
+    echo "sig_test:   ${cmd_sig_test}"
+    echo "batch_test: ${cmd_batch_test}"
+    echo "debug:      ${cmd_debug}"
+    echo "============> cmd <============"
 }
 
 function create_dir()
 {
-    if [ ! -d $1 ]; then echo "create dir $1"; mkdir -p $1; fi
+    [ ! -d $1 ] && { echo "create dir $1"; mkdir -p $1;}
+    [ ! -d $1 ] && { echo "create dir $1 faile"; return 1;} || return 0
 }
 
 function cdDir()
 {
     workDir=""
     case $1 in
-        "hevc")
-            workDir="${rootPath}/h265/HM/build"
-            ;;
-        "avc")
-            workDir="${rootPath}/h264/JM/build"
-            ;;
-        "avs2")
-            workDir="${rootPath}/avs2/RD17.0/build/linux"
-            ;;
-        "av1")
-            workDir="${rootPath}/av1/aom/Lbuild"
-            ;;
-        "vp9")
-            workDir="${rootPath}/vp9/libvpx/Lbuild"
-            ;;
-        *)
-            echo "unsupport prot: $1"
-            ;;
+        hevc) workDir="${rootPath}/h265/HM/build" ;;
+        avc)  workDir="${rootPath}/h264/JM/build" ;;
+        avs2) workDir="${rootPath}/avs2/RD17.0/build/linux" ;;
+        av1)  workDir="${rootPath}/av1/aom/Lbuild" ;;
+        vp9)  workDir="${rootPath}/vp9/libvpx/Lbuild" ;;
+        *)      echo "unsupport prot: $1"; help ;;
     esac
 
-    create_dir $workDir
-    if [ ! -d "$workDir" ]; then
-        echo "dir $workDir is not exist"
-        exit 0
-    else
-        cd $workDir
-    fi
+    create_dir ${workDir} && cd ${workDir} || { echo "dir ${workDir} is not exist"; exit 0;}
 }
 
 function build()
@@ -92,22 +91,13 @@ function build()
     cur_prot=$1
     cdDir $cur_prot
 
-    case $cur_prot in
-        "hevc")
-            ;;
-        "avc")
-            ;;
-        "avs2")
-            ;;
-        "av1")
-            cmake -DCMAKE_BUILD_TYPE=Debug ..
-            make
-            ;;
-        "vp9")
-            ;;
-        *)
-            echo "unsupport prot: $1"
-            ;;
+    case ${cur_prot} in
+        hevc) ;;
+        avc)  ;;
+        avs2) ;;
+        av1)  cmake -DCMAKE_BUILD_TYPE=Debug ..; make ;;
+        vp9)  ;;
+        *)      echo "unsupport prot: $1"; help ;;
     esac
 }
 
@@ -118,47 +108,52 @@ function run()
     cdDir $cur_prot
 
     cur_prot=$1
-    case $cur_prot in
-        "hevc")
-            hevcCmd=""
-            ;;
-        "avc")
-            avcCmd=""
-            ;;
-        "avs2")
-            avs2Cmd=""
-            ;;
-        "av1")
-            av1Cmd="./aomdec -o output.yuv ${HOME}/test/testStrms/Sintel_360_10s_1MB.ivf"
-            ;;
-        "vp9")
-            vp9Cmd=""
-            ;;
-        *)
-            echo "unsupport prot: $1"
-            ;;
+    case ${cur_prot} in
+        hevc) hevcCmd=${sig_hevc_run} ;;
+        avc)  avcCmd=${sig_avc_run}  ;;
+        avs2) avs2Cmd=${sig_avs2_run} ;;
+        av1)  av1Cmd=${sig_av1_run} ;;
+        vp9)  vp9Cmd=${sig_vp9_run}  ;;
+        *)    echo "unsupport prot: $1"; help ;;
     esac
 
     eval runCmd='$'${cur_prot}Cmd
-    if [ "${need_dbg}" == true ];then
-        gdb --command=debug.gdb --args $runCmd
-    else
-        $runCmd
-    fi
+    [ "${need_dbg}" == true ] && gdb --command=debug.gdb --args ${runCmd} || ${runCmd}
 
-    echo "cmd: $runCmd"
+    echo "sigle test cmd: ${runCmd}"
+}
+
+function run_batch()
+{
+    cur_prot=$1
+    cdDir $cur_prot
+
+    echo "need finish"
+    return 0
+
+    cur_prot=$1
+    case ${cur_prot} in
+        hevc) hevcCmd="" ;;
+        avc)  avcCmd=""  ;;
+        avs2) avs2Cmd="" ;;
+        av1)  av1Cmd=""  ;;
+        vp9)  vp9Cmd=""  ;;
+        *)    echo "unsupport prot: $1"; help ;;
+    esac
+
+    eval runCmd='$'${cur_prot}Cmd
+    [ "${need_dbg}" == true ] && gdb --command=debug.gdb --args ${runCmd} || ${runCmd}
+
+    echo "batch test cmd: ${runCmd}"
 }
 
 function main()
 {
     procParas $@
     
-    if [ $cmd_build == true ]; then
-        build $cmd_prot
-    fi
-    if [ $? -eq 0 ]; then
-        run $cmd_prot $cmd_debug
-    fi
+    [ ${cmd_build} == true ] && build ${cmd_prot}
+    [ ${cmd_sig_test} == true ] && run ${cmd_prot} ${cmd_debug}
+    [ ${cmd_batch_test} == true ] && run_batch ${cmd_prot}
 }
 
 # ============ main ============
